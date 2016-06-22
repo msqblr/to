db56e98df0ed0230bca3a88c726ce9cebfb75f44
Tatyanya <tatyanya@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2016-06-22 22:47:07 +0300

    test enabling of JIT compilation

---
+++ jit/src/main/java/org/company/App.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class App {
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        long test = 0;
+        for (int i = 0; i < 1000; i++) {
+            test +=i;
+        }
+        System.out.println(test);
+        long end  = System.nanoTime();
+        System.out.println(end - start);
+
+        start = System.nanoTime();
+        test = 0;
+        for (int i = 0; i < 10000000; i++) {
+            test +=i;
+        }
+        System.out.println(test);
+        end  = System.nanoTime();
+        System.out.println(end - start);
+    }
+}
